083471896fca91b85db4d66a0e5e43aa40f962c7
Isaac Porras <isaacpope@estudiantec.cr>
2020-08-10 20:45:54 -0600

    Con Login

--- CampusTEC/src/app/app.component.ts
+++ CampusTEC/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './Components/login/login.component.html',
+  styleUrls: ['./Components/login/login.component.css']
 })
 export class AppComponent {
   title = 'CampusTEC';
