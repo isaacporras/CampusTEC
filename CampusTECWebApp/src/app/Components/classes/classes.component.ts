ae507fc5a953ba64f14a72f8e6e0e0f942ac8dc4
Isaac Porras <isaacpope@estudiantec.cr>
2020-08-14 03:19:21 -0600

    Con vista de curso prelinimar

--- CampusTECWebApp/src/app/Components/classes/classes.component.ts
+++ CampusTECWebApp/src/app/Components/classes/classes.component.ts
@@ -1,4 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {FlatTreeControl} from '@angular/cdk/tree';
+
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Completar todas las asignaciones',
+    children: [
+      {name: 'Hacer tarea 1'},
+      {name: 'Hacer tarea 2'},
+      {name: 'Hacer tarea 3'},
+    ]
+  }, {
+    name: 'Ir a todas las tutorias',
+    children: [
+      {name: 'Ir a la tutoria 1'},
+      {name: 'Ir a la tutoria 2'},
+      {name: 'Ir a la tutoria 3'},
+    ]
+  }
+];
+
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
 
 
 
@@ -8,7 +41,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classes.component.css']
 })
 export class ClassesComponent implements OnInit {
-
+  private _transformer = (node: FoodNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  }
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level, node => node.expandable);
+    treeFlattener = new MatTreeFlattener(
+      this._transformer, node => node.level, node => node.expandable, node => node.children);
+      dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
   objectives: any = [
     {
       id: '1', description: 'Evaluar la conveniencia en el uso de un cierto método en lan solución de un problema numérico específico.'
@@ -25,8 +69,9 @@ export class ClassesComponent implements OnInit {
 
 
 
-  constructor() { }
-
+  constructor() { this.dataSource.data = TREE_DATA;}
+  
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
   ngOnInit() {
   }
 
