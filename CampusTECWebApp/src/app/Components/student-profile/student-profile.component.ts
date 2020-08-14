d168572a39693147aeb9e01b73a8b664e63f067c
Isaac Porras <isaacpope@estudiantec.cr>
2020-08-13 21:57:08 -0600

    Con informacion de perfil editable

--- CampusTECWebApp/src/app/Components/student-profile/student-profile.component.ts
+++ CampusTECWebApp/src/app/Components/student-profile/student-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-student-profile',
@@ -7,27 +9,111 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentProfileComponent implements OnInit {
 
-  studentData:any=
+  email1Disable: boolean = true;
+  email2Disable: boolean = true;
+  telNumberDisable: boolean = true;
+  universityDisable: boolean = true;
+  campusDisable: boolean = true;
+  editing: boolean = false;
+
+  studentBaseData: any;
+
+  studentDataForm: FormGroup;
+
+  studentClasses: any = [
     {
-      type: 'Estudent', nombre: 'Oscar', apellido: 'Porras', id: '2017107550', activo: 'Activo',
-      email1:'imanoisaaac1@gmail.com', email2: 'imanoisaaac23@gmail.com', numtelefono: '82837462',
-      universidad:'X-TEC', sede: 'Cartago'
-    };
-    studentClasses:any=[
-    {
-      id: '1',nombre:'Circuitos en Corriente continua'
+      id: '1', nombre: 'Circuitos en Corriente continua'
     },
     {
-      id: '2',nombre:'Circuitos en Corriente alterna'
+      id: '2', nombre: 'Circuitos en Corriente alterna'
     },
     {
-      id: '3',nombre:'Matematica'
+      id: '3', nombre: 'Matematica'
     }
-  
+
   ];
-  constructor() { }
+
+
+  onEdit(id) {
+    this.editing = true;
+    if (id === 1) {
+      this.studentDataForm.controls['email1'].enable();
+
+    }
+    else if (id == 2) {
+      this.studentDataForm.controls['email2'].enable();
+    }
+
+    else if (id == 3) {
+      this.studentDataForm.controls['telNumber'].enable();
+    }
+    else if (id == 4) {
+      this.studentDataForm.controls['university'].enable();
+    }
+    else if (id == 5) {
+      this.studentDataForm.controls['campus'].enable();
+    }
+  }
+
+  onSave() {
+
+    this.studentDataForm.controls['email1'].disable();
+    this.studentDataForm.controls['email2'].disable();
+    this.studentDataForm.controls['telNumber'].disable();
+    this.studentDataForm.controls['university'].disable();
+    this.studentDataForm.controls['campus'].disable();
+
+
+    console.log(JSON.stringify(this.studentDataForm.value, null, 4));
+    this.editing = false;
+
+  }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.studentDataForm = this.formBuilder.group({
+      email1: new FormControl({value:'', disable: true}, Validators.required),
+      email2: new FormControl({value:'', disable: true}, Validators.required),
+      telNumber: new FormControl({value:'', disable: true}, Validators.required),
+      university: new FormControl({value:'', disable: true}, Validators.required),
+      campus: new FormControl({value:'', disable: true}, Validators.required),
+    });
+
+
+    this.studentBaseData = {type: 'Estudent', nombre: 'Oscar', apellido: 'Porras', id: '2017107550', activo: 'Activo'};
+
+
+
+
+    this.studentDataForm.setValue({
+      email1: 'imanoisaaac1@gmail.com', email2: 'imanoisaaac23@gmail.com', telNumber: '82837462',
+      university: 'X-TEC', campus: 'Cartago'
+    });
+
+
+    this.studentDataForm.controls['email1'].disable();
+    this.studentDataForm.controls['email2'].disable();
+    this.studentDataForm.controls['telNumber'].disable();
+    this.studentDataForm.controls['university'].disable();
+    this.studentDataForm.controls['campus'].disable();
+
+
   }
+  changeEditingStatus() {
+
+    this.editing = true;
+
+  }
+
+  onDismiss(){
+    this.studentDataForm.controls['email1'].disable();
+    this.studentDataForm.controls['email2'].disable();
+    this.studentDataForm.controls['telNumber'].disable();
+    this.studentDataForm.controls['university'].disable();
+    this.studentDataForm.controls['campus'].disable();
+    this.editing = false;
+  }
+
 
 }
