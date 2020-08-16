c3cc39da26eae9e34e87260ecb6833c5e2f250dc
Kenneth Hernandez <khken21@gmail.com>
2020-08-16 15:09:58 -0600

    Aded profile total info query

--- CampusTEC-API/SQLFiles/SelectionQuerys/SelectionOfCourses.sql
+++ CampusTEC-API/SQLFiles/SelectionQuerys/SelectionOfCourses.sql
@@ -18,3 +18,101 @@ SELECT s.*
 FROM universidadpersona
          INNER JOIN universidad s on universidadpersona.IdUniversidad = s.IdUniversidad
 WHERE universidadpersona.IdPersona = ?;
+
+
+SELECT actividad.*
+FROM actividad
+         INNER JOIN actividadretoacademico ar
+WHERE ar.IdRetoAcademico = ?;
+
+SELECT A.idpersona,
+       A.nombre,
+       A.apellido,
+       A.activo,
+       A.numtelefono,
+       A.email1,
+       A.email2,
+       A.ppurl,
+       A.puesto,
+       universidad.Nombre
+FROM (
+         SELECT persona.*, universidadpersona.IdUniversidad
+         FROM persona
+                  INNER JOIN universidadpersona ON persona.IdPersona = universidadpersona.IdPersona
+         WHERE persona.IdPersona = ?
+     ) A
+         INNER JOIN universidad ON A.IdUniversidad = universidad.IdUniversidad;
+
+
+SELECT X.IdPersona,
+       X.Nombre,
+       X.Apellido,
+       X.Activo,
+       X.NumTelefono,
+       X.Email1,
+       X.Email2,
+       X.PpUrl,
+       X.Puesto,
+       X.Nombre,
+       sede.Nombre
+FROM (SELECT A.*, sedepersona.IdSede
+      FROM (
+               SELECT A.idpersona,
+                      A.nombre,
+                      A.apellido,
+                      A.activo,
+                      A.numtelefono,
+                      A.email1,
+                      A.email2,
+                      A.ppurl,
+                      A.puesto,
+                      universidad.Nombre AS universidadNombre
+               FROM (
+                        SELECT persona.*, universidadpersona.IdUniversidad
+                        FROM persona
+                                 INNER JOIN universidadpersona ON persona.IdPersona = universidadpersona.IdPersona
+                        WHERE persona.IdPersona = ?
+                    ) A
+                        INNER JOIN universidad ON A.IdUniversidad = universidad.IdUniversidad
+           ) A
+               INNER JOIN sedepersona ON A.IdPersona = sedepersona.IdPersona
+      WHERE sedepersona.IdPersona = ?) X
+         INNER JOIN sede ON X.IdSede = sede.IdSede;
+
+
+SELECT X.*,logininfo.Carne
+FROM (SELECT X.IdPersona,
+             X.Nombre,
+             X.Apellido,
+             X.Activo,
+             X.NumTelefono,
+             X.Email1,
+             X.Email2,
+             X.PpUrl,
+             X.Puesto,
+             X.universidadNombre,
+             sede.Nombre AS sedeNombre
+      FROM (SELECT A.*, sedepersona.IdSede
+            FROM (
+                     SELECT A.idpersona,
+                            A.nombre,
+                            A.apellido,
+                            A.activo,
+                            A.numtelefono,
+                            A.email1,
+                            A.email2,
+                            A.ppurl,
+                            A.puesto,
+                            universidad.Nombre AS universidadNombre
+                     FROM (
+                              SELECT persona.*, universidadpersona.IdUniversidad
+                              FROM persona
+                                       INNER JOIN universidadpersona ON persona.IdPersona = universidadpersona.IdPersona
+                              WHERE persona.IdPersona = ?
+                          ) A
+                              INNER JOIN universidad ON A.IdUniversidad = universidad.IdUniversidad
+                 ) A
+                     INNER JOIN sedepersona ON A.IdPersona = sedepersona.IdPersona
+            WHERE sedepersona.IdPersona = ?) X
+               INNER JOIN sede ON X.IdSede = sede.IdSede) X
+         INNER JOIN logininfo on logininfo.IdPersona = x.IdPersona
--- CampusTEC-API/src/DatabaseManagement/SelectQuerys/ProfileSelectQueries.java
+++ CampusTEC-API/src/DatabaseManagement/SelectQuerys/ProfileSelectQueries.java
@@ -29,6 +29,48 @@ public class ProfileSelectQueries {
         return statement.executeQuery();
     }
 
+    public static ResultSet getProfileInfoTotal(ArrayList<String> parameters, Connection con) throws SQLException {
+        PreparedStatement statement = con.prepareStatement("SELECT X.*,logininfo.Carne " +
+                "FROM (SELECT X.IdPersona, " +
+                "             X.Nombre, " +
+                "             X.Apellido, " +
+                "             X.Activo, " +
+                "             X.NumTelefono, " +
+                "             X.Email1, " +
+                "             X.Email2, " +
+                "             X.PpUrl, " +
+                "             X.Puesto, " +
+                "             X.universidadNombre, " +
+                "             sede.Nombre AS sedeNombre " +
+                "      FROM (SELECT A.*, sedepersona.IdSede " +
+                "            FROM ( " +
+                "                     SELECT A.idpersona, " +
+                "                            A.nombre, " +
+                "                            A.apellido, " +
+                "                            A.activo, " +
+                "                            A.numtelefono, " +
+                "                            A.email1, " +
+                "                            A.email2, " +
+                "                            A.ppurl, " +
+                "                            A.puesto, " +
+                "                            universidad.Nombre AS universidadNombre " +
+                "                     FROM ( " +
+                "                              SELECT persona.*, universidadpersona.IdUniversidad " +
+                "                              FROM persona " +
+                "                                       INNER JOIN universidadpersona ON persona.IdPersona = universidadpersona.IdPersona " +
+                "                              WHERE persona.IdPersona = ? " +
+                "                          ) A " +
+                "                              INNER JOIN universidad ON A.IdUniversidad = universidad.IdUniversidad " +
+                "                 ) A " +
+                "                     INNER JOIN sedepersona ON A.IdPersona = sedepersona.IdPersona " +
+                "            WHERE sedepersona.IdPersona = ?) X " +
+                "               INNER JOIN sede ON X.IdSede = sede.IdSede) X " +
+                "         INNER JOIN logininfo on logininfo.IdPersona = x.IdPersona");
+        statement.setInt(1, Integer.parseInt(parameters.get(0)));
+        statement.setInt(2, Integer.parseInt(parameters.get(0)));
+        return statement.executeQuery();
+    }
+
     public static ResultSet getTecColonesInfo(ArrayList<String> parameters, Connection con) throws SQLException {
         PreparedStatement statement = con.prepareStatement("SELECT * FROM cuenta where cuenta.IdPersona = ?;");
         statement.setInt(1, Integer.parseInt(parameters.get(0)));
@@ -43,6 +85,7 @@ public class ProfileSelectQueries {
         statement.setInt(1, Integer.parseInt(parameters.get(0)));
         return statement.executeQuery();
     }
+
     public static ResultSet getCampusFromPerson(ArrayList<String> parameters, Connection con) throws SQLException {
         PreparedStatement statement = con.prepareStatement("SELECT s.*  " +
                 "SELECT s.*  " +
