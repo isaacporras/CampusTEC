0c5e782116b282f9cb2fe6b3ca053bfe0b9cbd9f
Kenneth Hernandez <khken21@gmail.com>
2020-08-15 22:15:18 -0600

    Added more querys destined to adding to tables

--- CampusTEC-API/SQLFiles/AddQuerys/AddQuerys.sql
+++ CampusTEC-API/SQLFiles/AddQuerys/AddQuerys.sql
@@ -1,3 +1,13 @@
 INSERT INTO comentario( IdFile, IdActividad, Comentario, IdPersona, Fecha, Hora ) VALUES (?,?,?,?,?,?);
 INSERT INTO actividad( IdFile, NumSemana, TecColones, Fecha, Descripcion, IdCurso) VALUES (?,?,?,?,?,?);
-INSERT INTO retoacademico( Descripcion, Titulo, TecColones, IdCurso) VALUES (?,?,?,?)
+INSERT INTO retoacademico( Descripcion, Titulo, TecColones, IdCurso) VALUES (?,?,?,?);
+INSERT INTO retoacademicopersona(IdRetoAcademico, IdPersona, Completado) VALUES (?,?,?);
+INSERT INTO actividadpersona(IdActividad, IdPersona, Completado) VALUES (?,?,?);
+INSERT INTO retopsicologico( TecColones, Titulo) VALUES (?,?);
+INSERT INTO preguntapsico( idretopsicologico, pregunta) VALUES (?,?);
+INSERT INTO  respuesta(idpreguntapsico, respuesta) VALUES (?,?);
+INSERT INTO objetivo(descripcion, idcurso) VALUES (?,?);
+INSERT INTO tarea(idpersona, idactividad, titulo, descripcion, hora) VALUES (?,?,?,?,?);
+INSERT INTO respuestapregunta(idresultado, idpreguntapsico, idrespuesta) VALUES (?,?,?);
+INSERT INTO resultado( idretopsicologico) VALUES (?);
+SELECT LAST_INSERT_ID();
