f810e39c043a830aa9104f2e9dd7b9cb69c9e527
Kenneth Hernandez <khken21@gmail.com>
2020-08-18 15:24:57 -0600

    Added base for challenge test

--- CampusTEC-API/src/UnitTesting/ModelTesting/ChallengeViewTest.java
+++ CampusTEC-API/src/UnitTesting/ModelTesting/ChallengeViewTest.java
@@ -2,17 +2,14 @@ package UnitTesting.ModelTesting;
 
 import Model.ActivityView;
 import Model.ChallengeView;
-import Model.Objects.Activity;
-import Model.Objects.Challenge;
-import Model.Objects.Comment;
-import Model.Objects.Objective;
+import Model.Objects.*;
 import org.junit.jupiter.api.Test;
 
+import javax.json.JsonObject;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ChallengeViewTest {
     @Test
@@ -29,11 +26,34 @@ public class ChallengeViewTest {
         A.activities = new ArrayList<>();
         A.objectives = new ArrayList<>();
         A.children = new ArrayList<>();
-        //Tiene que hacer la lista de comentarios
+        //Tiene que hacer la lista
         assertEquals(x,x);
     }
 
-    
+    @Test
+    public void testgetObjectives() throws SQLException, ClassNotFoundException {
+        ArrayList<Objective> x = ChallengeView.getObjectives("1");
+        ArrayList<Objective> A = new ArrayList<Objective>();
+        //Tiene que hacer la lista
+        assertIterableEquals(x,A);
+    }
+
+    @Test
+    public void testgetStudents() throws SQLException, ClassNotFoundException {
+        ArrayList<User> x = ChallengeView.getStudents("1");
+        ArrayList<User> A = new ArrayList<User>();
+        //Tiene que hacer la lista
+        assertIterableEquals(x,A);
+    }
+
+
+    @Test
+    public void testupdateStudents() throws SQLException, ClassNotFoundException {
+        JsonObject request = null;
+        Boolean x = ChallengeView.updateStudents(request =);
+        //Tiene que hacer la lista
+        assertTrue(x);
+    }
 
 
 }
