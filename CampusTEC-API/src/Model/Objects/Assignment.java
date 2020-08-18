9a2e52ea3daeeebc6c87764cd383406491f4c607
Marco Herrera <m.herrera0799@gmail.com>
2020-08-18 10:16:27 -0600

    Add support for challenge completion

--- CampusTEC-API/src/Model/Objects/Assignment.java
+++ CampusTEC-API/src/Model/Objects/Assignment.java
@@ -1,13 +1,13 @@
 package Model.Objects;
 
 public class Assignment {
-    public String token;
-    public Integer week;
-    public String id;
-    public String name;
-    public String day;
-    public String time;
-    public boolean done;
-    public String description;
-    public String activity;
+    public String token = "";
+    public Integer week = 1;
+    public String id = "";
+    public String name = "";
+    public String day = "";
+    public String time = "";
+    public boolean done = false;
+    public String description = "";
+    public String activity = "";
 }
--- CampusTEC-API/src/Model/Objects/Objective.java
+++ CampusTEC-API/src/Model/Objects/Objective.java
@@ -1,7 +1,7 @@
 package Model.Objects;
 
 public class Objective {
-    public String id;
-    public String idClass;
-    public String description;
+    public String id = "";
+    public String idClass = "";
+    public String description = "";
 }
